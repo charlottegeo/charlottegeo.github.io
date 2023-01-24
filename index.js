bd6ffa60186111a2c711a16dc6d3bd168b7a0adb
Charlotte George <33436621+charlottegeo@users.noreply.github.com>
2023-01-24 17:36:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+tabs.forEach(tab => {
+    tab.addEventListener("click", event => {
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove("active");
+        });
+        const tabId = event.target.getAttribute("data-tab");
+        document.getElementById(tabId).classList.add("active");
+    });
+});
 window.onload = function () {
-            window.scrollTo(0,0)
+            window.scrollTo(0,0);
         };
